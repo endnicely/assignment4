99c8af1f8906379f536286d02b4bd869fd98440f
Jie Zhou <zhoujie66@yahoo.com>
2018-03-07 20:59:49 -0800

    completed version

--- beginning/js/02-inventory-management.js
+++ beginning/js/02-inventory-management.js
@@ -20,30 +20,21 @@ function sortFunction(a, b) {
     }
 }
 
-//function locStorage (invent) {
-//    "use strict";
-//    if(!localStorage.inventory) { 
-//                    
-//        localStorage.inventory = JSON.stringify(invent);
-//        
-//    } else {
-//        invent = JSON.parse(localStorage.inventory);
-//    }  
-//}
+function locStorage(invent) {
+    "use strict";
+    if (!localStorage.inventory) {
+        localStorage.inventory = JSON.stringify(invent);
+        
+    } else {
+        invent = JSON.parse(localStorage.inventory);
+        return invent;
+    }
+}
 
 function view(inventory) {
     "use strict";
-    inventory.sort(sortFunction); 
-    //locStorage(inventory);
-    if(!localStorage.inventory) { 
-                    
-        localStorage.inventory = JSON.stringify(inventory);
-        
-    } else {
-        inventory = JSON.parse(localStorage.inventory);
-    }  
-    
-    
+    inventory.sort(sortFunction);
+    inventory = locStorage(inventory);
     inventory.forEach(function (product) {
         window.console.log(product[0] + " " + product[1] + " (" + product[2] + ") $" + product[3]);
     });
@@ -51,42 +42,37 @@ function view(inventory) {
 
 function update(inventory) {
     "use strict";
+    inventory = locStorage(inventory);
     
-      if(!localStorage.inventory) { 
-                    
-        localStorage.inventory = JSON.stringify(inventory);
-        
-    } else {
-        inventory = JSON.parse(localStorage.inventory);
-    }  
-    
-    var  invalid_sku = false, sku = parseInt(window.prompt("Enter a product's sku number please!"), 10);
-   
-    while (isNaN(sku)) {
-        sku =  parseInt(window.prompt("The sku number should be a positive integer, enter a valid sku number please!"), 10);
-    }
-    
-    inventory.forEach(function (product) {
-        if (product[0] === sku) {
-            var i, quantity = parseInt(window.prompt("Enter a new stock quantity please!"), 10);
-            if (isNaN(quantity) ||  quantity < 0) {
-                quantity = parseInt(window.prompt("Please provide any non-negative integer for the quantity!"), 10);
-            }
+    var i, quantity, invalid_sku = true, sku = parseInt(window.prompt("Enter a product's sku number please!"), 10);
+    /* sku number validation */
+    do {
+        if (isNaN(sku)) {
+            sku =  parseInt(window.prompt("The sku number should be a positive integer, enter a valid sku number please!"), 10);
+        } else {
             for (i = 0; i < inventory.length; i += 1) {
                 if (inventory[i][0] === sku) {
-                    inventory[i][2] = quantity;
-                    return invalid_sku;
+                    invalid_sku = false;
+                    break;
                 }
             }
+            if (invalid_sku) {
+                sku =  parseInt(window.prompt("The sku number can't be found, enter a valid sku number please!"), 10);
+            }
         }
-        else {
-            sku =  parseInt(window.prompt("The sku number can't be found. Enter a valid sku number please!"), 10);
-        }
-    });
-      
-   localStorage.inventory = JSON.stringify(inventory);
+    } while (isNaN(sku) || invalid_sku);
     
-
+    /* stock quantity validation */
+    quantity = parseInt(window.prompt("Enter a new stock quantity please!"), 10);
+    while (isNaN(quantity) ||  quantity < 0) {
+        quantity = parseInt(window.prompt("Please provide any non-negative integer for the quantity!"), 10);
+    }
+    
+    /* update stock quantity for the item associated with the sku number */
+    inventory[i][2] = quantity;
+    /* update localStorage */
+    localStorage.inventory = JSON.stringify(inventory);
+    
 }
 
 
@@ -97,6 +83,7 @@ function main() {
     displayMenu();
     
     inventory = [[3223, "Hat", 12, 14.99], [2233, "Socks", 36, 9.99], [4824, "Shirt", 10, 15.99], [6343, "Jeans", 22, 39.99], [9382, "Jacket", 5, 49.99]];
+    
     while (true) {
         command = window.prompt("Enter command");
         if (command !== null) {
@@ -115,8 +102,6 @@ function main() {
         } else {
             break;
         }
-        
-        
     }
     window.console.log("Program terminated.");
     
